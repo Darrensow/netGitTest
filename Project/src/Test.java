c67b9abf0ea6dd1d618f8c2396c2dfb4748f6dc0
Darrensow <Darrensow@hotmail.com>
2021-02-02 02:53:41 +0800

    removed print line statements

--- Project/src/Test.java
+++ Project/src/Test.java
@@ -1,8 +1,5 @@
 public class Test{
     public static void main(String[] args) {
         System.out.println("test");
-        System.out.println("test123");
-        System.out.println("123");
-        System.out.println("testing 123");
     }
 }
