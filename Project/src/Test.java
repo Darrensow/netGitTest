18c2c781b9f952d1f3808176fd05984aa351320c
Darrensow <Darrensow@hotmail.com>
2021-02-02 03:02:56 +0800

    added methods'

--- Project/src/Test.java
+++ Project/src/Test.java
@@ -1,5 +1,14 @@
 public class Test{
     public static void main(String[] args) {
         System.out.println("test");
+        int num = 123;
+        System.out.println("Test number is " + 123);
+        printLines(100);
+        System.out.println();
+    }
+    public static void printLines(int n){
+        for (int i = 0; i < n; i++) {
+            System.out.print("_");
+        }
     }
 }
